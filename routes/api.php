91447fa677438550e7efbf62698ca9bd812fd6bd
yousufmj <yousufmjaleel@gmail.com>
2018-08-03 00:50:14 +0100

    add crud routes for api

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,22 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Business
+Route::prefix('business')->group(function () {
+    //Get all
+    Route::get('/', 'BusinessController@index');
+    //Get 1
+    Route::get('/{id}', 'BusinessController@index');
+    //Create
+    Route::post('/create', 'BusinessController@create');
+    //Edit
+    Route::put('/{id}', 'BusinessController@edit');
+    //Delete
+    Route::delete('/{id}', 'BusinessController@delete');
 });
